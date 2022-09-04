61b392efa8b263333e0385a32619718beced6a41
Acorn <github@unallied.com>
2022-09-04 10:31:54 -0700

    Move PageHeader out to its own component

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export function PageHeader() {
+  return (
+      <Head>
+        <title>Vote</title>
+        <meta name="description" content="Poll and question app. By Acorn1010. twitch.tv/acorn1010" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  );
+}
