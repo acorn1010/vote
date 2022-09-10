bd85f1cd9fb8cddbc6ae48a43920bda5b39a5092
Acorn <github@unallied.com>
2022-09-10 10:16:09 -0700

    Remove unused hello example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import {trpc} from "../utils/trpc";
-
 export default function Home() {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+  // const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   return (
     <p>
