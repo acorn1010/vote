9e3bdd25e50def05bc709df917512a02e74539db
Acorn <github@unallied.com>
2022-09-10 13:05:19 -0700

    Add / to non-Home paths

--- src/components/navigation/NavTitle.tsx
+++ src/components/navigation/NavTitle.tsx
@@ -3,10 +3,11 @@ import { useRouter } from 'next/router';
 export function NavTitle() {
   const { query } = useRouter();
 
+  const topic = query.topicId;
   return (
     <div className="flex flex-1 items-center justify-center">
       <div className="flex flex-shrink-0 items-center">
-        <h1>{query.topicId ?? 'Home'}</h1>
+        <h1>{topic ? `/${topic}` : 'Home'}</h1>
       </div>
     </div>
   );
