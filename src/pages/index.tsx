0a831dcb936a46a21b5b6a017a8294df750094c4
Acorn <github@unallied.com>
2022-09-10 11:56:55 -0700

    Fix next.js stupid rule about apostrophe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   // const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
-  return <p>Acorn1010's Board Members</p>;
+  return <p>{"Acorn1010's Board Members"}</p>;
 }
