119bea78b497f23a8828131d4b839ed782484e85
Acorn <github@unallied.com>
2022-09-11 12:50:56 -0700

    Remove GSSP from {topic}/create
    GSSP is slow because it requires a query to the server(!) on every request. This isn't necessary if the user is already signed in.

--- src/pages/[topicId]/create.tsx
+++ src/pages/[topicId]/create.tsx
@@ -1,27 +1,14 @@
-import { GetServerSidePropsContext } from 'next';
+import { useSession } from 'next-auth/react';
 import { Container } from '../../components/containers/Container';
-import { getServerAuthSession } from '../../server/common/get-server-auth-session';
 
 export default function Create() {
+  // Required: We want to ensure user has signed in so they don't waste time filling out a form
+  // that requires authentication to fill out!
+  useSession({ required: true });
+
   return (
     <Container>
       <p>TODO: Create a poll here.</p>
     </Container>
   );
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const topicId = '' + (context.query.topicId ?? '');
-  const session = await getServerAuthSession(context);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: `/login?forward=/${topicId}/create`,
-        permanent: false,
-      },
-    };
-  }
-
-  return { props: { session } };
 }
