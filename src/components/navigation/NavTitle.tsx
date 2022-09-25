ca832b681893d0d8c6acc3d345a148739b032d6e
Acorn <github@unallied.com>
2022-09-25 09:40:26 -0700

    Add link to page title
    Add a link to the page title to navigate back to that topic's home page.

--- src/components/navigation/NavTitle.tsx
+++ src/components/navigation/NavTitle.tsx
@@ -1,14 +1,28 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export function NavTitle() {
-  const { query } = useRouter();
-
-  const topic = query.topicId;
   return (
     <div className="flex flex-1 items-center justify-center">
       <div className="flex flex-shrink-0 items-center">
-        <h1>{topic ? `/${topic}` : 'Home'}</h1>
+        <h1 className="text-neutral-300 hover:text-white">
+          <TitleLink />
+        </h1>
       </div>
     </div>
   );
+}
+
+function TitleLink() {
+  const { query } = useRouter();
+  const topic = query.topicId;
+  if (topic) {
+    const url = `/${topic}`;
+    return (
+      <Link href={url} passHref>
+        <a>{url}</a>
+      </Link>
+    );
+  }
+  return <>Home</>;
 }
