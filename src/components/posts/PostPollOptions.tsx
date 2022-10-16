415417f9ffac43a179ae4b658b62a4d9ef159855
Acorn <github@unallied.com>
2022-10-16 13:44:39 -0700

    Hide percent on 0 votes
    If an option has 0 votes, hide the percent

--- src/components/posts/PostPollOptions.tsx
+++ src/components/posts/PostPollOptions.tsx
@@ -92,17 +92,17 @@ type PostPollOptionProps = {
 };
 function PostPollOption(props: PostPollOptionProps) {
   const { option, hasEnded, isWinner, totalVotes, variant, disabled, setIsSending } = props;
-  const percent = totalVotes
-    ? Math.round(Math.min(option.upvotesCount / totalVotes, 1) * 100)
-    : 100;
+  const percent = totalVotes ? Math.round(Math.min(option.upvotesCount / totalVotes, 1) * 100) : 0;
   const voteOption = trpc.post.voteOption.useMutation();
   const [hasVoted, setHasVoted] = useState(false); // True if user has voted on this option.
 
   const hasVotedOnOption = hasVoted || option.userVotes[0]?.pollOptionId === option.id;
+
+  const percentText = percent ? ` (${percent}%)` : '';
   return (
     <Tooltip
       key={option.id}
-      title={hasEnded ? `${isWinner ? '🏆 ' : ''}${option.upvotesCount} votes (${percent}%)` : ''}
+      title={hasEnded ? `${isWinner ? '🏆 ' : ''}${option.upvotesCount} votes${percentText}` : ''}
     >
       <Button
         className={clsx(
